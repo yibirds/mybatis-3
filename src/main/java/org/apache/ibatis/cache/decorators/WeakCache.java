bad5cc79a1dde88f80c37261b5abc150fdb3f909
yibirds <844718860@qq.com>
2020-06-16 22:08:42 +0800

    提交说明：cache 代码阅读

--- src/main/java/org/apache/ibatis/cache/decorators/WeakCache.java
+++ src/main/java/org/apache/ibatis/cache/decorators/WeakCache.java
@@ -29,9 +29,21 @@ import org.apache.ibatis.cache.Cache;
  * @author Clinton Begin
  */
 public class WeakCache implements Cache {
+  /**
+   * 强引用的键的队列
+   */
   private final Deque<Object> hardLinksToAvoidGarbageCollection;
+  /**
+   * 被 GC 回收的 WeakEntry 集合，避免被 GC。
+   */
   private final ReferenceQueue<Object> queueOfGarbageCollectedEntries;
+  /**
+   * 装饰的 Cache 对象
+   */
   private final Cache delegate;
+  /**
+   * {@link #hardLinksToAvoidGarbageCollection} 的大小
+   */
   private int numberOfHardLinks;
 
   public WeakCache(Cache delegate) {
